402403a8fd9ab2ccb24bf8c8424c4645eda6f077
Mihail Stoian <stoianmmihail@yahoo.com>
2021-10-01 22:17:14 +0200

    added feature vectors

---
+++ src/feature.py
@@ -0,0 +1,23 @@
+import numpy as np
+from bpemb import BPEmb
+from src.wordbox import Wordbox
+
+kNumSubwords = 3
+kDimension = 100
+kVocabularySize = 2e6
+
+def build_feature_vectors(boxes):
+  # Construct word embeddings with a large vocabulary size.
+  # Dismiss words with more than `kNumSubwords` subwords.
+  bpemb_en = BPEmb(lang='en', dim=kDimension, vs=kVocabularySize)
+
+  for box in boxes:
+    vs = bpemb_en.embed(box.text)
+    if vs.shape[0] > kNumSubwords:
+      # TODO: is this what we really want?
+      # Dismiss
+      continue
+    else:
+      embedding_vector = np.pad(np.asarray(vs.flat), (0, kNumSubwords * kDimension - vs.shape[0] * vs.shape[1]), 'constant', constant_values=(0, 0))
+      box.set_feature_vector(np.concatenate([embedding_vector, box.get_numeric_vector(), box.get_boolean_vector()]))
+  pass
